1f14b53321de73e3a6a130eb3171756da4abf376
Gaelle Ruf <gaelle.ruf@gmail.com>
2021-09-22 15:56:15 +0200

    [NEW] user methods GET and POST created

--- src/Controller/Api/V1/UserController.php
+++ src/Controller/Api/V1/UserController.php
@@ -2,10 +2,14 @@
 
 namespace App\Controller\Api\V1;
 
+use App\Entity\User;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 /**
  * @Route("/api/v1/users", name="api_v1_user")
@@ -16,21 +20,61 @@ class UserController extends AbstractController
      * URL : /api/v1/users
      * @Route : api_v1_user_index
      * 
-     * @Route("/",)
+     * @Route("/", name="index", methods={"GET"})
      */
-    public function index(): Response
+    public function index(UserRepository $userRepository): Response
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/Api/V1/UserController.php',
-        ]);
+        
+        $users = $userRepository->findAll();
+        
+        return $this->json($users, 200, [], []
+        
+    );
     }
 
-    public function show(): Response
+    /**
+     * 
+     * @Route("/{id}", name="show", methods={"GET"})
+     */
+    public function show(int $id, UserRepository $userRepository)
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/Api/V1/UserController.php',
-        ]);
+        $user = $userRepository->find($id);
+        
+        if(!$user) {
+            return $this->json([
+                'error' => 'L\'utilisateur ' . $id . ' n\'existe pas'
+            ], 404);
+        }
+
+        return $this->json($user, 200, [], []
+    );
     }
+
+     /**
+     * 
+     * URL : /api/v1/users/
+     * 
+     * @Route("/", name="add", methods={"POST"})
+     * 
+     * @return void
+     */
+    public function add(Request $request, SerializerInterface $serialiser, ValidatorInterface $validator)
+    {
+        // We get the json information
+        $jsonData = $request->getContent();
+
+        //We turn json data in object
+        $user = $serialiser->deserialize($jsonData, User::class, 'json');
+
+        //To save we call the manager
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($user);
+        $em->flush();
+
+        //We return an answer telling the ressource has been created with the 201 code.
+        return $this->json($user, 201, [], []);
+
+    
+    }
+
 }
