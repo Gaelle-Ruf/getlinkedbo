016231009a8fa153e53fbff1bc66c06d5c4a4136
AmauryBoulogne <amauryboulognesanti@gmail.com>
2021-09-24 16:29:04 +0200

    [UPDATE] Controllers and Templates Code : 1st Commit

--- src/Controller/Backoffice/CommentController.php
+++ src/Controller/Backoffice/CommentController.php
@@ -2,19 +2,92 @@
 
 namespace App\Controller\Backoffice;
 
+use App\Entity\Comment;
+use App\Form\CommentType;
+use App\Repository\CommentRepository;
+use DateTimeImmutable;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/backoffice/comment", name="backoffice_comment_", requirements={"id": "\d+"})
+ */
 class CommentController extends AbstractController
 {
     /**
-     * @Route("/backoffice/comment", name="backoffice_comment")
-     */
-    public function index(): Response
+    * @Route("/", name="index")
+    */
+    public function index(CommentRepository $commentRepository): Response
     {
         return $this->render('backoffice/comment/index.html.twig', [
-            'controller_name' => 'CommentController',
+            'comments' => $commentRepository->findAll(),]);
+    }
+
+    /**
+     * @Route("/{id}", name="show")
+     * @return Response
+     */
+    public function show(int $id, CommentRepository $commentRepository)
+    {
+        $comment = $commentRepository->find($id);
+        if (!$comment) {
+            throw $this->createNotFoundException('Le commentaire ' . $id . ' n\'existe pas');
+        }
+        return $this->render('backoffice/comment/show.html.twig', [
+            'comment' => $comment
         ]);
+    }
+
+    /**
+     * @Route("/add", name="add")
+     * @return Response
+     */
+    public function add(Request $request)
+    {
+        $comment = new Comment();
+        $form = $this->createForm(CommentType::class, $comment);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($comment);
+            $em->flush();
+            $this->addFlash('success', 'Le commentaire ' . $comment->getName() . ' a bien été créée');
+            return $this->redirectToRoute('backoffice_comment_index');
+        }
+        return $this->render('backoffice/comment/add.html.twig', [
+            'formView' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="edit")
+     * @return Response
+     */
+    public function edit(Comment $comment, Request $request)
+    {
+        $form = $this->createForm(CommentType::class, $comment);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $comment->setUpdatedAt(new DateTimeImmutable());
+            $this->getDoctrine()->getManager()->flush();
+            $this->addFlash('success', 'Le commentaire ' . $comment->getName() . ' a bien été modifiée');
+            return $this->redirectToRoute('backoffice_comment_show', ['id' => $comment->getId()]);
+        }
+        return $this->render('backoffice/comment/edit.html.twig', ['formView' => $form->createView()]);
+    }
+
+    /**
+     * @Route("/{id}/delete", name="delete")
+     * @return Response
+     */
+    public function delete(Comment $comment)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($comment);
+        $em->flush();
+        $this->addFlash('info', 'Le commentaire ' . $comment->getName() . ' a bien été supprimée');
+        return $this->redirectToRoute('backoffice_comment_index');
     }
 }
--- src/Controller/Backoffice/EventController.php
+++ src/Controller/Backoffice/EventController.php
@@ -2,19 +2,92 @@
 
 namespace App\Controller\Backoffice;
 
+use App\Entity\Event;
+use App\Form\EventType;
+use App\Repository\EventRepository;
+use DateTimeImmutable;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/backoffice/event", name="backoffice_event_", requirements={"id": "\d+"})
+ */
 class EventController extends AbstractController
 {
     /**
-     * @Route("/backoffice/event", name="backoffice_event")
-     */
-    public function index(): Response
+    * @Route("/", name="index")
+    */
+    public function index(EventRepository $eventRepository): Response
     {
         return $this->render('backoffice/event/index.html.twig', [
-            'controller_name' => 'EventController',
+            'events' => $eventRepository->findAll(),]);
+    }
+
+    /**
+     * @Route("/{id}", name="show")
+     * @return Response
+     */
+    public function show(int $id, EventRepository $eventRepository)
+    {
+        $event = $eventRepository->find($id);
+        if (!$event) {
+            throw $this->createNotFoundException('L\'événement ' . $id . ' n\'existe pas');
+        }
+        return $this->render('backoffice/event/show.html.twig', [
+            'event' => $event
         ]);
+    }
+
+    /**
+     * @Route("/add", name="add")
+     * @return Response
+     */
+    public function add(Request $request)
+    {
+        $event = new Event();
+        $form = $this->createForm(EventType::class, $event);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($event);
+            $em->flush();
+            $this->addFlash('success', 'L\'événement ' . $event->getName() . ' a bien été créée');
+            return $this->redirectToRoute('backoffice_event_index');
+        }
+        return $this->render('backoffice/event/add.html.twig', [
+            'formView' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="edit")
+     * @return Response
+     */
+    public function edit(Event $event, Request $request)
+    {
+        $form = $this->createForm(EventType::class, $event);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $event->setUpdatedAt(new DateTimeImmutable());
+            $this->getDoctrine()->getManager()->flush();
+            $this->addFlash('success', 'L\événement ' . $event->getName() . ' a bien été modifiée');
+            return $this->redirectToRoute('backoffice_event_show', ['id' => $event->getId()]);
+        }
+        return $this->render('backoffice/event/edit.html.twig', ['formView' => $form->createView()]);
+    }
+
+    /**
+     * @Route("/{id}/delete", name="delete")
+     * @return Response
+     */
+    public function delete(Event $event)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($event);
+        $em->flush();
+        $this->addFlash('info', 'L\événement ' . $event->getName() . ' a bien été supprimée');
+        return $this->redirectToRoute('backoffice_event_index');
     }
 }
