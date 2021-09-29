993ef51a6bc8f1c0b6ef49a1dcd304d221376cc5
Gaelle Ruf <gaelle.ruf@gmail.com>
2021-09-29 17:28:38 +0200

    [UPDATE] api login

--- src/Controller/Api/V1/SecurityController.php
+++ src/Controller/Api/V1/SecurityController.php
@@ -7,10 +7,13 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
+/**
+ * @Route("/api/v1")
+ */
 class SecurityController extends AbstractController
 {
     /**
-     * @Route("/login", name="app_login")
+     * @Route("/login", name="login")
      */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
