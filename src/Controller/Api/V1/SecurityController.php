c0688422894f69d865e3a89d04c338ae70f22000
Gaelle Ruf <gaelle.ruf@gmail.com>
2021-09-30 11:05:04 +0200

    [UPDATE] authentication login

--- src/Controller/Api/V1/SecurityController.php
+++ src/Controller/Api/V1/SecurityController.php
@@ -17,16 +17,17 @@ class SecurityController extends AbstractController
      */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        if ($this->getUser()) {  
+        /* if ($this->getUser()) {  
              return $this->redirectToRoute('target_path');
-        }
+        } */
 
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        // TODO return token infos users
+        /* return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]); */
     }
 
     /**
