71743d662887c8cb795750dd1a3263eddd05509f
Gaelle Ruf <gaelle.ruf@gmail.com>
2021-10-01 13:31:20 +0200

    [UPDATE] detail of returned datas

--- src/EventListener/AuthenticationSuccessListener.php
+++ src/EventListener/AuthenticationSuccessListener.php
@@ -23,9 +23,29 @@ class AuthenticationSuccessListener{
         }
 
         $data['data'] = array(
-            'email' =>  $user->getEmail(),
+            'type' =>  $user->getType(),
+            'name' =>  $user->getName(),
             'firstname' =>  $user->getFirstname(),
             'lastname' =>  $user->getLastname(),
+            'picture' =>  $user->getPicture(),
+            'description' =>  $user->getDescription(),
+            'schedule' =>  $user->getSchedule(),
+            'nbMembers' =>  $user->getNbMembers(),
+            'address' =>  $user->getAddress(),
+            'website' =>  $user->getWebsite(),
+            'facebook' =>  $user->getFacebook(),
+            'instagram' =>  $user->getInstagram(),
+            'twitter' =>  $user->getTwitter(),
+            'email' =>  $user->getEmail(),
+            'password' =>  $user->getPassword(),
+            'slug' =>  $user->getSlug(),
+            'createdAt' =>  $user->getCreatedAt(),
+            'updatedAt' =>  $user->getUpdatedAt(),
+            'events' =>  $user->getEvents(),
+            'category' =>  $user->getCategory(),
+            'style' =>  $user->getStyle(),
+            'comment' =>  $user->getComment(),
+            'participation' =>  $user->getParticipation(),
         );
 
         $event->setData($data);
